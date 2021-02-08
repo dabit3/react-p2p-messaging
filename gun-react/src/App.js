bd0e8401704da7bc790cbd99e56a4f096b1210a2
Nader Dabit <dabit3@gmail.com>
2021-02-07 22:05:15 -0600

    updated to add description to fields

--- gun-react/src/App.js
+++ gun-react/src/App.js
@@ -1,16 +1,19 @@
 import { useEffect, useState, useReducer } from 'react'
 import Gun from 'gun'
 
+// initialize gun locally
 const gun = Gun({
   peers: [
     'http://localhost:3030/gun'
   ]
 })
 
+// create the initial state to hold the messages
 const initialState = {
   messages: []
 }
 
+// Create a reducer that will update the messages array
 function reducer(state, message) {
   return {
     messages: [message, ...state.messages]
@@ -18,21 +21,28 @@ function reducer(state, message) {
 }
 
 export default function App() {
-  const [formState, setForm] = useState({
+  // the form state manages the form input for creating a new message
+    const [formState, setForm] = useState({
     name: '', message: ''
   })
+
+  // initialize the reducer & state for holding the messages array
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  // when the app loads, fetch the current messages and load them into the state
+  // this also subscribes to new data as it changes and updates the local state
   useEffect(() => {
     const messages = gun.get('messages')
     messages.map().on(m => {
       dispatch({
-        message: m.message,
         name: m.name,
+        message: m.message,
         createdAt: m.createdAt
       })
     })
   }, [])
-  
+
+  // set a new message in gun, update the local state to reset the form field
   function saveMessage() {
     const messages = gun.get('messages')
     messages.set({
@@ -43,35 +53,37 @@ export default function App() {
     setForm({
       name: '', message: ''
     })
-  } 
+  }
+
+  // update the form state as the user types
   function onChange(e) {
     setForm({ ...formState, [e.target.name]: e.target.value  })
   }
-  console.log('formState: ', formState)
+
   return (
     <div style={{ padding: 30 }}>
-        <input
-          onChange={onChange}
-          placeholder="Name"
-          name="name"
-          value={formState.name}
-        />
-        <input
-          onChange={onChange}
-          placeholder="Message"
-          name="message"
-          value={formState.message}
-        />
-        <button onClick={saveMessage}>Send Message</button>
-        {
-          state.messages.map(message => (
-            <div key={message.createdAt}>
-              <h2>{message.message}</h2>
-              <h3>{message.name}</h3>
-              <p>{message.createdAt}</p>
-            </div>
-          ))
-        }
+      <input
+        onChange={onChange}
+        placeholder="Name"
+        name="name"
+        value={formState.name}
+      />
+      <input
+        onChange={onChange}
+        placeholder="Message"
+        name="message"
+        value={formState.message}
+      />
+      <button onClick={saveMessage}>Send Message</button>
+      {
+        state.messages.map(message => (
+          <div key={message.createdAt}>
+            <h2>{message.message}</h2>
+            <h3>From: {message.name}</h3>
+            <p>Date: {message.createdAt}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
